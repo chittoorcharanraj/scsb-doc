6bc8ac972fa1651b3bc866bcda78ca8bd500be3d
rajeshbabuk <rajeshbabu.k@htcindia.com>
2021-11-01 23:12:02 +0530

    Ongoing MA - Changed Anamoly update query

--- src/main/java/org/recap/repository/jpa/BibliographicDetailsRepository.java
+++ src/main/java/org/recap/repository/jpa/BibliographicDetailsRepository.java
@@ -318,7 +318,10 @@ public interface BibliographicDetailsRepository extends BaseRepository<Bibliogra
     int resetMAQualifierForGrouping(@Param("bibliographicIds")List<Integer> bibIds);
 
     @Modifying
-    @Query(value = "UPDATE `BIBLIOGRAPHIC_T` AS a inner JOIN (select distinct MATCHING_IDENTITY from `BIBLIOGRAPHIC_T` where BIBLIOGRAPHIC_ID in (:bibliographicIds) group by MATCHING_IDENTITY, MATCH_SCORE having count(MATCHING_IDENTITY) = 1) AS b ON a.MATCHING_IDENTITY = b.MATCHING_IDENTITY set ANAMOLY_FLAG = 1",nativeQuery = true)
+    @Query(value = "UPDATE `BIBLIOGRAPHIC_T` AS a " +
+            "inner JOIN (select distinct(b1.MATCHING_IDENTITY) from bibliographic_t b1, bibliographic_t b2 " +
+            "where b1.MATCH_SCORE <> b2.MATCH_SCORE and b1.MATCHING_IDENTITY = b2.MATCHING_IDENTITY and b1.BIBLIOGRAPHIC_ID in (:bibliographicIds) " +
+            "group by b1.MATCHING_IDENTITY ) AS b ON a.MATCHING_IDENTITY = b.MATCHING_IDENTITY", nativeQuery = true)
     @Transactional
     int updateAnamolyFlagForBibIds(@Param("bibliographicIds")List<Integer> bibIds);
 }
