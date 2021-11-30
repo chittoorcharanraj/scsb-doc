4586e60adf076b1cf7ced28dcaebc05473d034cf
rmanimaranhtc <manimaran.r@htcindia.com>
2021-11-30 16:11:41 +0530

    UT_Changes

---
+++ src/test/java/org/recap/service/TitleMatchReportExportServiceExceptionUT.java
@@ -0,0 +1,61 @@
+package org.recap.service;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.recap.BaseTestCaseUT4;
+import org.recap.model.reports.TitleMatchedReport;
+import org.recap.util.CsvUtil;
+import org.recap.util.ReportsServiceUtil;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author dinakar on 30/11/21
+ */
+public class TitleMatchReportExportServiceExceptionUT extends BaseTestCaseUT4 {
+
+    @Mock
+    TitleMatchReportExportService titleMatchReportExportService;
+
+    @Mock
+    CsvUtil csvUtil;
+
+    @Mock
+    ReportsServiceUtil reportsServiceUtil;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    public CsvUtil getCsvUtil() {
+        return csvUtil;
+    }
+
+    @Test
+    public void generateTitleMatchReport() throws SolrServerException, IOException, ParseException {
+        TitleMatchedReport titleMatchedReport = new TitleMatchedReport();
+        titleMatchedReport.setOwningInst("PUL");
+        titleMatchedReport.setTotalPageCount(2);
+        titleMatchedReport.setTotalRecordsCount(1048500);
+        titleMatchedReport.setTitleMatch("test1");
+        ReflectionTestUtils.setField(titleMatchReportExportService, "titleReportDir", "org/recap/service");
+        ReflectionTestUtils.setField(titleMatchReportExportService, "reportsServiceUtil",reportsServiceUtil);
+        ReflectionTestUtils.setField(titleMatchReportExportService, "titleReportStatusFileName","org/recap/service/LoadStatusCompleted.txt");
+
+        Mockito.when(titleMatchReportExportService.getCsvUtil()).thenReturn(csvUtil);
+        Mockito.when(reportsServiceUtil.getTitleMatchedReportsExportS3(Mockito.any())).thenReturn(titleMatchedReport);
+        Mockito.when(titleMatchReportExportService.getCsvUtil().createTitleMatchReportFile(Mockito.anyString(),Mockito.any())).thenThrow(new IllegalArgumentException());
+        Mockito.when(titleMatchReportExportService.process(titleMatchedReport)).thenCallRealMethod();
+        TitleMatchedReport response = titleMatchReportExportService.process(titleMatchedReport);
+        assertNotNull(response);
+    }
+}
